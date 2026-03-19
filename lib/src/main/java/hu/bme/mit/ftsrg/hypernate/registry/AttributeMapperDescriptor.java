a95f71ff9e596a511578ab1ec6dca121991a2ef6
Ádám Györgyfalvai <gya2005@gmail.com>
2026-03-19 08:50:10 +0100

    KeyClass defined, Basic methods Implemented

--- lib/src/main/java/hu/bme/mit/ftsrg/hypernate/registry/AttributeMapperDescriptor.java
+++ lib/src/main/java/hu/bme/mit/ftsrg/hypernate/registry/AttributeMapperDescriptor.java
@@ -3,4 +3,17 @@ package hu.bme.mit.ftsrg.hypernate.registry;
 public class AttributeMapperDescriptor {
     private AttributeDescriptor forAttr;
     private String mapperClassName;
+
+    public AttributeMapperDescriptor(AttributeDescriptor attrdesc, String mapperName) {
+        this.forAttr = attrdesc;
+        this.mapperClassName = mapperName;
+    }
+
+    public AttributeDescriptor getAttributeDescriptor() {
+        return forAttr;
+    }
+
+    public String getMapperName() {
+        return mapperClassName;
+    }
 }
--- lib/src/main/java/hu/bme/mit/ftsrg/hypernate/registry/EntityMetaInventory.java
+++ lib/src/main/java/hu/bme/mit/ftsrg/hypernate/registry/EntityMetaInventory.java
@@ -2,11 +2,36 @@ package hu.bme.mit.ftsrg.hypernate.registry;
 
 import java.util.Set;
 
+import hu.bme.mit.ftsrg.hypernate.annotations.PrimaryKey;
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfoList;
+import io.github.classgraph.ScanResult;
+
 public class EntityMetaInventory {
     private Set<EntityMeta> data;
+
+    public EntityMetaInventory() {
+        initialize();
+    }
 
     public void add(EntityMeta meta) {
         data.add(meta);
     }
 
+    public EntityMeta getForClass(Class<?> clazz) {
+        for (EntityMeta em : data) {
+            if (em.getClassName().equals(clazz.getSimpleName())) {
+                return em;
+            }
+        }
+        return null;
+    }
+
+    public static void initialize() {
+        try (ScanResult result = new ClassGraph().enableClassInfo().enableAnnotationInfo().scan()) {
+            ClassInfoList classinfo = result.getClassesWithAnnotation(PrimaryKey.class);
+        } catch (Exception e) {
+
+        }
+    }
 }
