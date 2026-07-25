519a79e0c06017a86b4305c217253a98f2150ee7
Ádám Györgyfalvai <gya2005@gmail.com>
2026-07-25 21:36:45 +0200

    refactor: delete unused MissingEntityMetadataException class
    Signed-off-by: Ádám Györgyfalvai <gya2005@gmail.com>

--- lib/src/main/java/hu/bme/mit/ftsrg/hypernate/registry/MissingEntityMetadataException.java
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: Apache-2.0 */
-package hu.bme.mit.ftsrg.hypernate.registry;
-
-import hu.bme.mit.ftsrg.hypernate.HypernateException;
-import lombok.experimental.StandardException;
-
-/** Thrown when metadata for a specific entity class cannot be found. */
-@StandardException
-public class MissingEntityMetadataException extends HypernateException {}
